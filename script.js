d32c659dcf8f92cf287886baad792afffacf93f4
Rai <43397919+Rtv4pp@users.noreply.github.com>
2022-07-04 20:21:21 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function ep1 (){
       window.scrollTo( 1000, 0 );
-      document.getElementById("myVid").src = "https://rr1---sn-cgpn5oxu-o5ul.googlevideo.com/videoplayback?expire=1656963628&ei=rNHCYtG4NsySkAO6t5T4CA&ip=177.37.197.186&id=e8012196e17a0829&itag=18&source=blogger&mh=Oi&mm=31&mn=sn-cgpn5oxu-o5ul&ms=au&mv=m&mvi=1&pl=23&susc=bl&eaua=SInkzHz54lM&mime=video/mp4&vprv=1&dur=1477.183&lmt=1603672531584948&mt=1656934392&txp=1319224&sparams=expire,ei,ip,id,itag,source,susc,eaua,mime,vprv,dur,lmt&sig=AOq0QJ8wRAIgZ3qlZ90aBqWOElUOXgOcfEH7wuT7M6gd1nei8Xkl2IACIA3pjp_YNspU633tVga9gaZ877NY9eMal_egl8Tiqjl9&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIhANae2J-8kurfkfTgyjdprIjSOlr7LK_LeWeAcIrmHLBZAiAPWEVT1FXl93GJWh_nx2rw5LMFZVDumxYT_bQnARrnTw%3D%3D&cpn=sD02gCpDjBaPG-J6&c=WEB_EMBEDDED_PLAYER&cver=1.20220629.01.00";
-      document.getElementById("tp").textContent="New Text";
+      document.getElementById("myVid").src = "https://videos.animesgratisbr.com/pubfolder/animes/Onepiece/1.mp4";
+      document.getElementById("tp").textContent="EP 1";
       }
 function ep2 (){
       window.scrollTo( 1000, 0 );
