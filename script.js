e8d01fcc3374ad411969d5ee3065c7cbb357627c
Rai <43397919+Rtv4pp@users.noreply.github.com>
2022-07-04 08:11:22 -0300

    update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ $('.linkvideo').click(function(){
    var link = $(this).attr('id');
    switch(link){ 
       case 'ep1':
-	$('#myVid').attr('src', 'https://www.w3schools.com/tags/movie.mp4');
+	$('#myVid').attr('src', 'https://videos.animesgratisbr.com/pubfolder/animes/Onepiece/1.mp4');
 	$("#divVideo video")[0].load();
         break;
       case 'ep2':
-         $('#myVid').attr('src', 'https://www.w3schools.com/html/mov_bbb.mp4');
+         $('#myVid').attr('src', 'https://videos.animesgratisbr.com/pubfolder/animes/Onepiece/2.mp4');
 	 $("#divVideo video")[0].load();
          break;
     }
