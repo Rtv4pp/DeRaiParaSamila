2f13aba4522e8fffb6d3dee61d7ac7ca48076dc1
Rai <43397919+Rtv4pp@users.noreply.github.com>
2022-07-04 08:28:43 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-$('.linkvideo').click(function(){
-   var link = $(this).attr('id');
-   switch(link){ 
-      case 'ep1':
-	$('#myVid').attr('src', 'https://videos.animesgratisbr.com/pubfolder/animes/Onepiece/1.mp4');
-	$("#divVideo video")[0].load();
-        break;
-      case 'ep2':
-         $('#myVid').attr('src', 'https://videos.animesgratisbr.com/pubfolder/animes/Onepiece/2.mp4');
-	 $("#divVideo video")[0].load();
-         break;
-    }
-					
-});
+function ep1 (){
+      window.scrollTo( 1000, 0 );
+      document.getElementById("myVid").src = "https://videos.animesgratisbr.com/pubfolder/animes/Onepiece/1.mp4";
+      document.getElementById("tp").innerHTML="Episódio 929292";
+      }
+function ep2 (){
+      window.scrollTo( 1000, 0 );
+      document.getElementById("myVid").src = "https://videos.animesgratisbr.com/pubfolder/animes/Onepiece/2.mp4";
+      }
+function ep3 (){
+      window.scrollTo( 1000, 0 );
+      document.getElementById("myVid").src = "https://videos.animesgratisbr.com/pubfolder/animes/Onepiece/3.mp4";
+      }
+function ep4 (){
+      window.scrollTo( 1000, 0 );
+      document.getElementById("myVid").src = "https://videos.animesgratisbr.com/pubfolder/animes/Onepiece/4.mp4";
+      }
+function ep5 (){
+      window.scrollTo( 1000, 0 );
+      document.getElementById("myVid").src = "https://videos.animesgratisbr.com/pubfolder/animes/Onepiece/5.mp4";
+      }
+function ep6 (){
+      window.scrollTo( 1000, 0 );
+      document.getElementById("myVid").src = "https://videos.animesgratisbr.com/pubfolder/animes/Onepiece/6.mp4";
+      }
+function ep7 (){
+      window.scrollTo( 1000, 0 );
+      document.getElementById("myVid").src = "https://videos.animesgratisbr.com/pubfolder/animes/Onepiece/7.mp4";
+      }
+function ep8 (){
+      window.scrollTo( 1000, 0 );
+      document.getElementById("myVid").src = "https://videos.animesgratisbr.com/pubfolder/animes/Onepiece/8.mp4";
+      }
