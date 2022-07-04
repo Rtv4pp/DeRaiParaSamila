308e9176f2f4f16e6538d063caf264138ed5676e
Rai <43397919+Rtv4pp@users.noreply.github.com>
2022-07-04 08:01:31 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+$('.linkvideo').click(function(){
+   var link = $(this).attr('id');
+   switch(link){ 
+      case 'ep1':
+	$('#myVid').attr('src', 'https://www.w3schools.com/tags/movie.mp4');
+	$("#divVideo video")[0].load();
+        break;
+      case 'ep2':
+         $('#myVid').attr('src', 'https://www.w3schools.com/html/mov_bbb.mp4');
+	 $("#divVideo video")[0].load();
+         break;
+    }
+					
+});
